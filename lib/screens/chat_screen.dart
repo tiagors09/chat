445c8063d77d0a7c0d7f9148536c19f69b18e11f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-06-04 09:31:51 -0300

    chore: Adde some code to chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, i) => Container(
+          padding: const EdgeInsets.all(10),
+          child: const Text('Funcionou!'),
+        ),
+      ),
+    );
+  }
+}
